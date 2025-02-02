7bf4b7d927a8672a14930bbdc458bbb174d5881c
Phoenix100X <37727760+Phoenix100X@users.noreply.github.com>
2025-02-01 23:36:53 -0500

    Copy src/pages/bookings/index.tsx from Phoenix100X.github.io

---
+++ src/pages/bookings/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BookingCalendar } from './BookingCalendar';
+
+export function Bookings() {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <BookingCalendar />
+    </div>
+  );
+}
