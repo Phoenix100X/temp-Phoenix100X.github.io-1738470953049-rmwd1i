421dbbf23d7fe2c0a67e32598f67c6407a13d40a
Phoenix100X <37727760+Phoenix100X@users.noreply.github.com>
2025-02-01 23:36:57 -0500

    Copy src/pages/children/index.tsx from Phoenix100X.github.io

---
+++ src/pages/children/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ChildrenList } from './ChildrenList';
+
+export function Children() {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <ChildrenList />
+    </div>
+  );
+}
